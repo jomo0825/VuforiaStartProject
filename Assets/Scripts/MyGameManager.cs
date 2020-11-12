ea504fd502513e5595c3f10dfbf9f36bb2770f6e
jomo0825 <jomo0825@gmail.com>
2020-11-13 01:57:54 +0800

    20201113 0157

---
+++ Assets/Scripts/MyGameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGameManager : MonoBehaviour
+{
+    public AudioClip clipToPlay;
+
+    private AudioSource asource;
+
+    void Awake()
+    {
+        asource = GetComponent<AudioSource>();
+        if (asource == null)
+        {
+            asource = gameObject.AddComponent<AudioSource>();
+        }
+        asource.spatialBlend = 0;
+    }
+
+    public void OnButtonClicked() {
+        if (clipToPlay != null && asource != null)
+        {
+            asource.clip = clipToPlay;
+            asource.Play();
+        }
+    }
+}
