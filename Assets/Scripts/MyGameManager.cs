bc119349aff284d8eb17a72f0953b6126887ac03
jomo0825 <jomo0825@gmail.com>
2020-11-13 11:07:34 +0800

    20201113 1107

--- Assets/Scripts/MyGameManager.cs
+++ Assets/Scripts/MyGameManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyGameManager : MonoBehaviour
 {
     public AudioClip clipToPlay;
+    public Text txt;
+    public int lineNum = 1;
+    public List<int> answers;
 
     private AudioSource asource;
 
@@ -16,13 +20,44 @@ public class MyGameManager : MonoBehaviour
             asource = gameObject.AddComponent<AudioSource>();
         }
         asource.spatialBlend = 0;
+
+        answers = new List<int>();
     }
 
-    public void OnButtonClicked() {
+    void Update() {
+
+        if (lineNum == 1)
+        {
+            txt.text = "這是第一題:太陽的英文是甚麼? A. Sun B. Moon C. Star";
+        }
+        else if (lineNum == 2)
+        {
+            txt.text = "這是第二題";
+        }
+        else if (lineNum == 3)
+        {
+            txt.text = "這是第三題";
+        }
+        else if (lineNum == 4)
+        {
+            txt.text = "這是第四題";
+        }
+        else if (lineNum == 5)
+        {
+            txt.text = "這是第五題";
+        }
+
+    }
+
+
+    public void OnButtonClicked(int choice) {
         if (clipToPlay != null && asource != null)
         {
             asource.clip = clipToPlay;
             asource.Play();
         }
+
+        lineNum = lineNum + 1;
+        answers.Add(choice);
     }
 }
